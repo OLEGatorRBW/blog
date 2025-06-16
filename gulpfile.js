f9d65010a8066ed3f8e960110927a561bbd6b01d
OLEGatroRBW <OLEGatroRBW@gmail.com>
2025-06-16 11:14:52 +0300

    feat(build): update Gulp configuration and dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,11 @@ const { src, dest, parallel, series, watch } = require('gulp');
 const fileInclude = require('gulp-file-include');
 const sass = require('gulp-sass')(require('sass'));
 const browserSync = require('browser-sync').create();
-const plumber = require('gulp-plumber');
-const notify = require('gulp-notify');
-const fs = require('fs');
+const imagemin = require('gulp-imagemin');
+const newer = require('gulp-newer');
+const webp = require('gulp-webp'); // Оставляем оригинальное имя
+const size = require('gulp-size');
+const del = require('del');
 
 // Пути
 const paths = {
@@ -12,7 +14,7 @@ const paths = {
     html: 'src/html/**/*.html',
     scss: 'src/scss/**/*.scss',
     js: 'src/js/**/*.js',
-    img: 'src/img/**/*.{jpg,jpeg,png,gif,svg,webp}'
+    img: 'src/img/**/*.{jpg,jpeg,png,gif,svg}'
   },
   dist: {
     base: 'dist/',
@@ -23,86 +25,95 @@ const paths = {
   }
 };
 
-// Очистка dist (исправленная версия)
+// Очистка dist
 function clean() {
-  if (fs.existsSync(paths.dist.base)) {
-    fs.rmSync(paths.dist.base, { recursive: true, force: true });
-  }
-  return Promise.resolve();
+  return del([paths.dist.base]);
+}
+
+// Оптимизация изображений
+function images() {
+  return src(paths.src.img)
+    .pipe(newer(paths.dist.img))
+    .pipe(imagemin([
+      imagemin.mozjpeg({ quality: 80 }),
+      imagemin.optipng({ optimizationLevel: 5 }),
+      imagemin.svgo({
+        plugins: [
+          { removeViewBox: false },
+          { cleanupIDs: false }
+        ]
+      })
+    ]))
+    .pipe(size({ showFiles: true }))
+    .pipe(dest(paths.dist.img))
+    .pipe(browserSync.stream());
+}
+
+// Конвертация в WebP (исправленная версия)
+function convertToWebp() {
+  return src(paths.src.img)
+    .pipe(newer({
+      dest: paths.dist.img,
+      ext: '.webp'
+    }))
+    .pipe(webp({ 
+      quality: 80,
+      method: 6 // Добавляем явное указание метода сжатия
+    }))
+    .pipe(dest(paths.dist.img))
+    .pipe(browserSync.stream());
 }
 
 // HTML
 function html() {
   return src(paths.src.html)
-    .pipe(plumber())
-    .pipe(fileInclude({
-      prefix: '@@',
-      basepath: '@file'
-    }))
+    .pipe(fileInclude())
     .pipe(dest(paths.dist.html))
     .pipe(browserSync.stream());
 }
 
-// SCSS
+// SCSS -> CSS
 function styles() {
-  if (!fs.existsSync(paths.dist.css)) {
-    fs.mkdirSync(paths.dist.css, { recursive: true });
-  }
-
   return src(paths.src.scss)
-    .pipe(plumber({
-      errorHandler: notify.onError({
-        title: "SCSS Error",
-        message: "<%= error.message %>",
-        sound: false
-      })
-    }))
     .pipe(sass().on('error', sass.logError))
     .pipe(dest(paths.dist.css))
-    .pipe(browserSync.stream({ match: '**/*.css' }));
+    .pipe(browserSync.stream());
 }
 
-// JavaScript
+// JS
 function scripts() {
   return src(paths.src.js)
     .pipe(dest(paths.dist.js))
     .pipe(browserSync.stream());
 }
 
-// Изображения
-function images() {
-  return src(paths.src.img)
-    .pipe(dest(paths.dist.img))
-    .pipe(browserSync.stream());
-}
-
 // Сервер
 function serve() {
   browserSync.init({
-    server: {
-      baseDir: paths.dist.base,
-      serveStaticOptions: {
-        extensions: ['html']
-      }
-    },
+    server: { baseDir: paths.dist.base },
     notify: false,
-    open: true
+    port: 3000
   });
 
-  watch(paths.src.html, html).on('change', browserSync.reload);
+  watch(paths.src.html, html);
   watch(paths.src.scss, styles);
   watch(paths.src.js, scripts);
-  watch(paths.src.img, images);
+  watch(paths.src.img, series(images, convertToWebp));
 }
 
 // Сборка
-const build = series(clean, parallel(html, styles, scripts, images));
+const build = series(
+  clean,
+  parallel(
+    html,
+    styles,
+    scripts,
+    series(images, convertToWebp)
+  )
+);
 
-// Экспорт задач
-exports.clean = clean;
-exports.html = html;
-exports.styles = styles;
-exports.scripts = scripts;
+// Экспорт
 exports.images = images;
+exports.webp = convertToWebp;
 exports.build = build;
 exports.default = series(build, serve);
